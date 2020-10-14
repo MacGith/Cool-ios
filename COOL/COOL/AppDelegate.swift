192d008f3adc4d55ef8464d49368ed87fc083f18
Anand Praveen <anand.praveen@sephora.com>
2020-10-14 07:09:14 -0700

    google sign-in is implemented with Firebase.

--- COOL/COOL/AppDelegate.swift
+++ COOL/COOL/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,11 +15,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
+        
         // Override point for customization after application launch.
         window = UIWindow()
-        window?.rootViewController = LoginRegister()
+        window?.rootViewController = CoolSignInViewController()
         window?.makeKeyAndVisible()
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        return GIDSignIn.sharedInstance().handle(url)
     }
 }
 
+extension AppDelegate: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("Failed to log into Google:", error)
+        }
+        
+        guard  let idToken = user.authentication.idToken else {
+            return
+        }
+        guard let accessToken = user.authentication.accessToken else {
+            return
+        }
+        
+        let credentials = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        
+       
+        Auth.auth().signIn(with: credentials) { [weak self] (result, error) in
+            if let error = error {
+                print("Failed to create a Firebase User with Google Account: ", error)
+                return
+            }
+            
+            guard let uid = result?.user.uid else {
+                return
+            }
+            print("Sucessfully logged into firebase with google:", uid)
+  
+            self?.launchTabBar()
+        }
+    }
+    
+    private func launchTabBar() {
+        let tabBar = MainTabBar()
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        appDelegate?.window?.rootViewController = tabBar
+    }
+}
---
+++ COOL/COOL/Signin/CoolSignInViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CoolSignInViewController.swift
+//  COOL
+//
+//  Created by Praveen, Anand on 10/4/20.
+//
+
+import UIKit
+
+import GoogleSignIn
+
+class CoolSignInViewController: UIViewController {
+    
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        
+        configureLoginUI()
+    }
+    
+    /// Configure the UI elements of the Login/Register screen.
+    ///
+    /// A guidance label, buttons linking social media methods to login.
+    fileprivate func configureLoginUI() {
+        //if user previously sign launch home
+        if isPreviouslySignIn() {
+            launchHome()
+        }
+    }
+    
+    @IBAction func googleSignInTapped() {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    func isPreviouslySignIn() -> Bool {
+        guard let isSignIn = GIDSignIn.sharedInstance()?.hasPreviousSignIn(), isSignIn else {
+            return false
+        }
+        return true
+    }
+    
+    func launchHome() {
+        let tabBar = MainTabBar()
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        appDelegate?.window?.rootViewController = tabBar
+    }
+    
+}
